bce5c412074eb474fe120e61c3c43906ba459b27
Aketzalli Garcia <a299437@uach.mx>
2021-05-27 17:47:53 -0600

    Proyecto funcional :)

--- proyecto2/src/App.vue
+++ proyecto2/src/App.vue
@@ -1,13 +1,103 @@
 <template>
   <div>
-    Hola mundo.egg
+    <form @submit.prevent="estatusEditar ? actualizar() : crearLibro()">
+      <p>
+        <input type="text" v-model="titulo" placeholder="Título">
+        <input type="text" v-model="autor" placeholder="Autor">
+        <button type="submit">{{estatusEditar ? "Actualizar" : "Agregar"}}</button>
+        <button v-if="estatusEditar" @click="cancelaEdicion()">Cancelar</button>
+      </p>
+    </form>
+
+    <br>
+    <img src="https://cdn2.scratch.mit.edu/get_image/gallery/289341_170x100.png" v-if="cargando">
+
+    <table>
+      <thead>
+        <tr>
+          <th>Título</th>
+          <th>Autor</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tr v-for="libro in libros" :key="libro.titulo">
+        <td>{{libro.titulo}}</td>
+        <td>{{libro.autor}}</td>
+        <button @click="borrarLibro(libro)">Eliminar</button>
+        <button @click="seleccionar(libro)">Editar</button>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+import {db} from './firebase'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      libros:[],
+      id: "",
+      titulo: "",
+      autor: "",
+      cargando: false,
+      estatusEditar: false,
+    }
+  },
+  created(){
+    this.listarLibros();
+  },
+  methods:{
+    async listarLibros(){
+      this.cargando = true;
+      const data = await db.collection("libros").get();
+      this.libros = data.docs.map(doc => ({
+        id: doc.id, ...doc.data()
+      }))
+      this.cargando = false;
+      console.log(this.libros)
+    },
+    async crearLibro(){
+      await db.collection('libros').add({
+        titulo: this.titulo,
+        autor: this.autor,
+      })
+      this.titulo = "";
+      this.autor = "";
+      this.listarLibros();
+    },
+    async borrarLibro(libro){
+      if (confirm(`¿Estás seguro que desear eliminar el libro ${libro.titulo}?`)){
+        await db.collection('libros').doc(libro.id).delete();
+        this.listarLibros();
+      }
+    },
+    seleccionar(libro){
+      this.estatusEditar = true;
+      this.id = libro.id;
+      this.titulo = libro.titulo;
+      this.autor = libro.autor;
+    },
+    async actualizar(){
+      await db.collection('libros').doc(this.id).set({
+        titulo: this.titulo,
+        autor: this.autor
+      })
+      this.estatusEditar = false;
+      this.id = ""
+      this.titulo = ""
+      this.autor = ""
+      this.listarLibros();
+    },
+    cancelaEdicion(){
+      this.estatusEditar = false;
+      this.id = ""
+      this.titulo = ""
+      this.autor = ""
+    }
+  },
 }
 </script>
 
